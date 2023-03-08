4eda90a4e6f63e6340e8c1fba5e398fb6015372f
kzkzkzkzk <116419385+kzkzkzkzk@users.noreply.github.com>
2023-03-08 15:21:21 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,33 +1,74 @@
-const express = require("express");
-const { webhookCallback } = require("grammy");
-
 const TelegramApi = require("node-telegram-bot-api");
+const token = "6150726433:AAGJVYxtENl3jVgPQOT9Pm2A3bjeEzd99n8";
+const port = process.env.PORT || 443;
+const host = process.env.HOST || "0.0.0.0";
+const externalUrl = "https://naebshchik.vercel.app";
+const bot = new TelegramApi(token, {
+  webHook: { port: port, host: host },
+});
+bot.setWebHook(`${externalUrl}:${port}/bot${token}`);
 
-const token = "5982640356:AAEhL1azXkVJmA0I2VqHiB-aVDcVJCCX0gM";
+// const bot = new TelegramApi(token, {
+//   polling: true,
+// });
 
-const bot = new TelegramApi(token, { polling: true });
+const channels = [
+  { name: "Валентин или Андрей блять", chatId: "valentin_a_mojet_andrei" },
+  { name: "Все таки Андрей блять", chatId: "vse_taki_andrei_blyad" },
+];
 
 const users = {};
+const defaultUserInfo = { money: 0 };
+const minMoney = 50;
+const currency = "р.";
 
-const a = async (chatId) => {
-  users[chatId] = users[chatId] || {
-    money: 0,
-  };
+const isUserInChannel = (member) =>
+  (member.status === "member" ||
+    member.status === "administrator" ||
+    member.status === "creator") &&
+  !member.user.is_bot;
+
+const isSubscribed = async (chatId) => {
+  try {
+    const results = await Promise.all(
+      channels.map((el) => bot.getChatMember(`@${el.chatId}`, chatId))
+    );
+    for (const r of results) {
+      if (!isUserInChannel(r)) {
+        return false;
+      }
+    }
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+const start = async (chatId) => {
+  users[chatId] = users[chatId] || defaultUserInfo;
+  const subscribed = await isSubscribed(chatId);
   return bot.sendMessage(
     chatId,
-    "*Регистрация почти завершена! 😎*\n\nЧтобы начать зарабатывать необходимо быть подписанным на следующие спонсорские каналы 📩",
+    subscribed
+      ? "*Регистрация завершена! 😎*\n\nЗарабатывай 💵"
+      : "*Регистрация почти завершена! 😎*\n\nЧтобы начать зарабатывать 💵 необходимо быть подписанным на спонсорские каналы 📩",
     {
       reply_markup: {
         inline_keyboard: [
           [
-            {
-              text: "Подписаться",
-              callback_data: "subscribe",
-            },
+            subscribed
+              ? {
+                  text: "Вывести деньги 💵",
+                  callback_data: "sendmoney",
+                }
+              : {
+                  text: "Подписаться 📩",
+                  callback_data: "subscribe",
+                },
           ],
           [
             {
-              text: "Заработать",
+              text: "Заработать 💵",
               callback_data: "makemoney",
             },
           ],
@@ -38,71 +79,130 @@ const a = async (chatId) => {
   );
 };
 
-const start = () => {
-  bot.on("message", async (msg) => {
+const stop = async (chatId) => {
+  const subscribed = await isSubscribed(chatId);
+  return bot.sendMessage(
+    chatId,
+    subscribed
+      ? "Спасибо что подписался на спонсорские каналы 📩 теперь можешь выводить деньги 💵"
+      : "Чтобы вывести деньги 💵 необходимо быть подписанным на следующие спонсорские каналы 📩",
+    {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            subscribed
+              ? {
+                  text: "Вывести деньги 💵",
+                  callback_data: "sendmoney",
+                }
+              : {
+                  text: "Подписаться 📩",
+                  callback_data: "subscribe",
+                },
+          ],
+          [
+            {
+              text: "Заработать 💵",
+              callback_data: "makemoney",
+            },
+          ],
+        ],
+      },
+      parse_mode: "Markdown",
+    }
+  );
+};
+
+const makemoney = (chatId) => {
+  return bot.sendPhoto(
+    chatId,
+    `https://fakeface.rest/face/view?a=${new Date()}`,
+    {
+      caption: "Это мужчина или женщина? 🤔",
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "Мужчина 👨‍🦰", callback_data: "answer1" }],
+          [{ text: "Женщина 👱‍♀️", callback_data: "answer2" }],
+          [{ text: "А может Андрей блять? 🐓", callback_data: "answer2" }],
+          [{ text: "Стоп 🛑", callback_data: "stop" }],
+        ],
+      },
+    }
+  );
+};
+
+const answer = async (chatId) => {
+  const user = users[chatId] || defaultUserInfo;
+  const money = +(Math.random() * 10).toFixed(2);
+  user.money = +(user.money + money).toFixed(2);
+  await bot.sendMessage(
+    chatId,
+    `Ты заработал ${money}${currency} 💵\nТвой баланс ${user.money}${currency} 💵`,
+    { parse_mode: "Markdown" }
+  );
+  return makemoney(chatId);
+};
+
+const subscribe = (chatId) => {
+  return bot.sendMessage(chatId, "Спонсорские каналы 📩", {
+    reply_markup: {
+      inline_keyboard: [
+        ...channels.map((el) => [
+          {
+            text: `${el.name} 📩`,
+            url: `https://t.me/${el.chatId}`,
+          },
+        ]),
+        [{ text: "🔙", callback_data: "stop" }],
+      ],
+    },
+  });
+};
+
+const sendmoney = (chatId) => {
+  const user = users[chatId] || defaultUserInfo;
+  return bot.sendMessage(
+    chatId,
+    user.money < minMoney
+      ? `Минимальная сумма для выплаты ${minMoney}${currency} 💵 У тебя ${user.money}${currency} 💵`
+      : "Ай иди ты нахуй 😁",
+    {
+      reply_markup: {
+        inline_keyboard: [[{ text: "Ок", callback_data: "stop" }]],
+      },
+    }
+  );
+};
+
+const botStart = () => {
+  bot.on("message", (msg) => {
     const text = msg.text;
     const chatId = msg.chat.id;
     if (text === "/start") {
-      return a(chatId);
+      return start(chatId);
     }
-    return bot.sendMessage(
-      chatId,
-      "Я тебя не понимаю долбоеб, попробуй еще раз!)"
-    );
+    return bot.sendMessage(chatId, "Я тебя не понимаю, попробуй еще раз!)");
   });
 
-  bot.on("callback_query", async (msg) => {
+  bot.on("callback_query", (msg) => {
     const data = msg.data;
     const chatId = msg.message.chat.id;
-    if (data === "/start") {
-      return a(chatId);
+    if (data === "stop") {
+      return stop(chatId);
     }
     if (data === "subscribe") {
-      return bot.sendMessage(chatId, "Ай иди ты нахуй");
+      return subscribe(chatId);
     }
     if (data === "makemoney") {
-      await bot.sendPhoto(
-        chatId,
-        `https://fakeface.rest/face/view?a=${new Date()}`,
-        {
-          caption: "Это мужчина или женщина? 🤔",
-          reply_markup: {
-            inline_keyboard: [
-              [{ text: "Мужчина", callback_data: "0000000000" }],
-              [{ text: "Женщина", callback_data: "1111111111" }],
-            ],
-          },
-        }
-      );
+      return makemoney(chatId);
     }
-    if (data === "0000000000" || data === "1111111111") {
-      const user = users[chatId];
-      if (user) {
-        user.money += 1;
-        return bot.sendMessage(chatId, `Твой баланс ${user.money}р.`, {
-          reply_markup: JSON.stringify({
-            inline_keyboard: [
-              [{ text: "Продолжить", callback_data: "makemoney" }],
-              [{ text: "Вывести деньги", callback_data: "sendmoney" }],
-            ],
-          }),
-        });
-      }
+    if (data.startsWith("answer")) {
+      return answer(chatId);
     }
     if (data === "sendmoney") {
-      return bot.sendMessage(chatId, "Ай иди ты нахуй", {
-        reply_markup: {
-          inline_keyboard: [[{ text: "Ок", callback_data: "makemoney" }]],
-        },
-      });
+      return sendmoney(chatId);
     }
   });
 };
 
-const app = express();
-app.use(webhookCallback(start(), "express"));
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Bot listening on port ${PORT}`);
-});
+botStart();
